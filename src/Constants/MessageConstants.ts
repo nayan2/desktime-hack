f856e972657d6e942d5235c756001df571897c82
chowdhury nayan <nayanchowdhury92@gmail.com>
2020-09-09 01:48:11 +0600

    Project structure change and user seeting added

---
+++ src/Constants/MessageConstants.ts
@@ -0,0 +1,4 @@
+export const appMessages = {
+    inActiveWorkSpace: 'No Workspace Is Current Active, PLease Open A Workspace Then Try Again!',
+    inActiveExtension: 'PLease Start the Extension First!'
+};
