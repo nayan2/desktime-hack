561377fafd890df9b42a683cc501ce7299be31c8
chowdhury nayan <nayanchowdhury92@gmail.com>
2020-09-11 23:40:33 +0600

    bug fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,109 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
 import * as path from 'path';
 import * as url from 'url';
+import * as nut from '@nut-tree/nut-js';
 import { Helper } from './Helper/helper';
 import { LoremIpsumService } from './Services/loremIpsumService';
 import { AppConfiguration } from './configuration/appConfiguration';
 import { appMessages } from './Constants/AppConstants';
-import { keyboard, Key, mouse, left, right, up, down, screen } from '@nut-tree/nut-js';
 
 export async function activate(context: vscode.ExtensionContext) {
+
+	// what kind of problem you are solving
+	// why you have choosen this way
 
 	/*
 		* Global variable declaration
 	*/
 	let isProcessActive: boolean = true;
+	let workspaceFiles: string[] = Helper.getallFiles(Helper.currentWorkSpacePath, [], AppConfiguration.filterFileTypes);
 
 	let active = vscode.commands.registerCommand('desktime-hack.startDHack', async () => {
 
 		if (!Helper.currentWorkSpacePath) return vscode.window.showErrorMessage(appMessages.inActiveWorkSpace);
 
-		while(isProcessActive) {
-			if(AppConfiguration.switchBetweenRandomAndAllExisting) {		
-				const createdFilePath: string = await Helper.createAFile(path.join(url.fileURLToPath(Helper.currentWorkSpacePath), `${ Helper.currentTimeStamp() }.${ AppConfiguration.filterFileTypes[0] }`));
-				Helper.openInEditor(createdFilePath);
-				let fileData: any | string = await new LoremIpsumService().getRandomText();
-				fileData = Helper.inspectTextGeneatorAPIResult().containObjectResult ? Helper.extractPropFromObject(fileData.data) : fileData.data;
+		let actWindow = await nut.getActiveWindow();
+		console.log(actWindow.title);
 
-			} else {
+		// while(isProcessActive) {
+			
+		// 	/*
+		// 		* Takeing initiative for mouse activity
+		// 	*/
+		// 	if(AppConfiguration.mouseActive) {
 				
-			}
+		// 	}
 
-		// 	// 	
+		// 	/*
+		// 		* 
+		// 	*/
+		// 	if(AppConfiguration.switchBetweenRandomAndAllExisting) {
+				
+		// 		let istraverseActive: boolean = true;
 
-		// 	// 	while(isProcessActive) {
-		// 	// 		// let fileData: any | string = await new LoremIpsumService().getRandomText();
-		// 	// 		// fileData = fileData.data;
-					
-		// 	// 		// while(fileData.length >= 1) {
-		// 	// 		// 	let firstLine: string = fileData.split("\n").shift();
-		// 	// 		// 	//fs.writeFileSync(filePath, firstLine);
-		// 	// 		// 	await keyboard.type(firstLine);
-		// 	// 		// 	fileData = Helper.removeFirstLine(fileData);
-		// 	// 		// 	await Helper.sleep(2000);
-		// 	// 		// 	fs.writeFileSync(filePath, "");
-		// 	// 		// 	await Helper.sleep(3000);
-		// 	// 		// }
-		// 	// 	}
+		// 		setTimeout(() => { istraverseActive = false; }, AppConfiguration.traverseDelay * 60000);
 
-		// 	// 	// Close the editor
-		// 	// 	vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		// 		const createdFilePath: string = await Helper.createAFile(path.join(url.fileURLToPath(Helper.currentWorkSpacePath), `${ Helper.currentTimeStamp() }.${ AppConfiguration.filterFileTypes[0] }`));
+		// 		Helper.openInEditor(createdFilePath);
 
-		// 	// 	// Delete file
-		// 	// 	vscode.workspace.fs.delete(vscode.Uri.file(filePath));
+		// 		while(istraverseActive) {
+		// 			let randonText: any | string = await new LoremIpsumService().getRandomText();
+		// 			randonText = Helper.inspectTextGeneatorAPIResult().containObjectResult ? Helper.extractPropFromObject(randonText.data) : randonText.data;
+		// 			await nut.keyboard.type(randonText);
+		// 		}
+
+		// 		// Close the editor
+		// 		vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+
+		// 		// Delete file
+		// 		vscode.workspace.fs.delete(vscode.Uri.file(createdFilePath));
+
+		// 		while(istraverseActive) {
+		// 			Helper.openInEditor(workspaceFiles[Math.ceil((workspaceFiles.length * Math.random()) / 1)]);
+		// 			let randonText: any | string = await new LoremIpsumService().getRandomText();
+		// 			randonText = Helper.inspectTextGeneatorAPIResult().containObjectResult ? Helper.extractPropFromObject(randonText.data) : randonText.data;
+		// 			await nut.keyboard.type(randonText);
+		// 		}
+
+		// 		// Close the editor
+		// 		vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		// 	} else if(AppConfiguration.traverseOnlyRandomFiles) {
+
+		// 		let istraverseActive: boolean = true;
+
+		// 		setTimeout(() => { istraverseActive = false; }, AppConfiguration.traverseDelay * 60000);
+
+		// 		const createdFilePath: string = await Helper.createAFile(path.join(url.fileURLToPath(Helper.currentWorkSpacePath), `${ Helper.currentTimeStamp() }.${ AppConfiguration.filterFileTypes[0] }`));
+		// 		Helper.openInEditor(createdFilePath);
+
+		// 		while(istraverseActive) {
+		// 			let randonText: any | string = await new LoremIpsumService().getRandomText();
+		// 			randonText = Helper.inspectTextGeneatorAPIResult().containObjectResult ? Helper.extractPropFromObject(randonText.data) : randonText.data;
+		// 			await nut.keyboard.type(randonText);
+		// 		}
+
+		// 		// Close the editor
+		// 		vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+
+		// 		// Delete file
+		// 		vscode.workspace.fs.delete(vscode.Uri.file(createdFilePath));
+		// 	} else if(AppConfiguration.traverseOnlyExisting) {
+		// 		let istraverseActive: boolean = true;
+
+		// 		setTimeout(() => { istraverseActive = false; }, AppConfiguration.traverseDelay * 60000);
+
+		// 		workspaceFiles.forEach(async x => {
+		// 			while(istraverseActive) {
+		// 				Helper.openInEditor(x);
+		// 				let randonText: any | string = await new LoremIpsumService().getRandomText();
+		// 				randonText = Helper.inspectTextGeneatorAPIResult().containObjectResult ? Helper.extractPropFromObject(randonText.data) : randonText.data;
+		// 				await nut.keyboard.type(randonText);
+		// 			}
+
+		// 			// Close the editor
+		// 			vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		// 		});
+		// 	}
 		// }
 	});
 
